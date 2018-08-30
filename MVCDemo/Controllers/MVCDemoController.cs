7651b59ee732528cb737cc20573c1bef27acd167
dhyana1984 <xiongyi_1984@foxmail.com>
2018-08-30 14:31:49 +0800

    部分视图的学习，包括@Html.Partial, @Html.RenderPartial和@Html.Action, @Html.RenderAction

--- MVCDemo/Controllers/MVCDemoController.cs
+++ MVCDemo/Controllers/MVCDemoController.cs
@@ -13,5 +13,23 @@ namespace MVCDemo.Controllers
         {
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult ShowWidget()
+        {
+            return PartialView("~/Views/Shared/_PartialPageWidget.cshtml");
+        }
+
+        public ActionResult SharedDateDemo()
+        {
+            ViewBag.DateTime = DateTime.Now;
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult PartialViewDate()
+        {
+            ViewBag.DateTime = DateTime.Now.AddMinutes(10);
+            return PartialView("_PartialPageDateTime");
+        }
     }
 }
